262afd8ef78a22de2c080e5a78363c829f88665b
piraultjd <piraultjd@vcu.edu>
2018-01-16 23:40:34 -0500

    - added ajax call to /players

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 (function () {
 const getUsername = document.getElementById('human');
 const username = document.getElementById('username');
-getUsername.addEventListener("click", function sendUsername(data) {
+getUsername.addEventListener("click", function sendUsername(evt) {
+    evt.preventDefault();
     console.log(username.value);
     const XHR = new XMLHttpRequest();
     XHR.open('POST', '/players');
-    XHR.send();
+    XHR.send(JSON.stringify({username: username.value}));
   });
 })();
 
